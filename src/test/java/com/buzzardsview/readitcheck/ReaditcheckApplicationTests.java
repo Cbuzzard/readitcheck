4190af09ccf3c1b102f416861b21ad384c874e2a
cbuzzard <corwin1998@gmail.com>
2020-09-11 17:46:04 -0400

    init

---
+++ src/test/java/com/buzzardsview/readitcheck/ReaditcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzzardsview.readitcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaditcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
