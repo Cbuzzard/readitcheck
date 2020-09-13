986ffa051ff2414a12a7f5b2cf415bccce559a32
cbuzzard <corwin1998@gmail.com>
2020-09-12 22:11:37 -0400

    added request filters

---
+++ src/main/java/com/buzzardsview/readitcheck/config/filters/LoginFilter.java
@@ -0,0 +1,65 @@
+package com.buzzardsview.readitcheck.config.filters;
+
+import com.buzzardsview.readitcheck.security.AppTokenProvider;
+import com.buzzardsview.readitcheck.security.GoogleTokenVerifier;
+import com.buzzardsview.readitcheck.security.exception.InvalidTokenException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginFilter implements Filter {
+
+    private GoogleTokenVerifier googleTokenVerifier;
+
+    @Autowired
+    public LoginFilter(GoogleTokenVerifier googleTokenVerifier) {
+        this.googleTokenVerifier = googleTokenVerifier;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        String idToken = ((HttpServletRequest) servletRequest).getHeader("X-ID-TOKEN");
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (idToken != null) {
+            final Payload payload;
+            try {
+                payload = googleTokenVerifier.verify(idToken);
+                if (payload != null) {
+                    String username = payload.getSubject();
+                    String name = (String) payload.get("name");
+                    request.setAttribute("name", name);
+                    request.setAttribute("userId", username);
+                    AppTokenProvider.addAuthentication(response, username);
+                    filterChain.doFilter(servletRequest, response);
+                    return;
+                }
+            } catch (GeneralSecurityException | InvalidTokenException e) {
+                // This is not a valid token, we will send HTTP 401 back
+            }
+        }
+        ((HttpServletResponse) servletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
