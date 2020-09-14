3619fb562c14905d48974bd9e3d75c53a76d5a7f
cbuzzard <corwin1998@gmail.com>
2020-09-13 20:19:45 -0400

    method to post comment if approved

--- src/main/java/com/buzzardsview/readitcheck/controller/CommentController.java
+++ src/main/java/com/buzzardsview/readitcheck/controller/CommentController.java
@@ -2,13 +2,42 @@ package com.buzzardsview.readitcheck.controller;
 
 import com.buzzardsview.readitcheck.data.CommentRepository;
 import com.buzzardsview.readitcheck.data.SubmissionRepository;
+import com.buzzardsview.readitcheck.data.UserRepository;
 import com.buzzardsview.readitcheck.model.Comment;
+import com.buzzardsview.readitcheck.model.Submission;
+import com.buzzardsview.readitcheck.model.User;
+import com.buzzardsview.readitcheck.model.dto.CommentPostDto;
+import com.buzzardsview.readitcheck.security.AppTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletRequest;
 
 @RestController
-@RequestMapping("comment")
+@RequestMapping("rest/submission/{submissionId}/comment")
 public class CommentController {
+
+    //TODO add custom submission not found exception
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private SubmissionRepository submissionRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public void newComment(@RequestBody CommentPostDto commentPostDto, @PathVariable Integer submissionId, ServletRequest servletRequest) {
+        Submission submission = submissionRepository.getById(submissionId).orElseThrow();
+        User user = userRepository.findById(AppTokenProvider.getCurrentUserId(servletRequest)).orElseThrow();
+
+        if (submission.getApprovedUsers().contains(user)) {
+            Comment newComment = new Comment(user, commentPostDto.getContent(), submission);
+            submission.addComment(newComment);
+            commentRepository.save(newComment);
+            submissionRepository.save(submission);
+        }
+
+    }
 
 }
