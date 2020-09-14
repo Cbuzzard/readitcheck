0f1be7b6299ed8f08d5b60b03442ad1aba1ed43a
cbuzzard <corwin1998@gmail.com>
2020-09-13 20:21:10 -0400

    returns SubmissionGetDto with approval info

--- src/main/java/com/buzzardsview/readitcheck/controller/SubmissionController.java
+++ src/main/java/com/buzzardsview/readitcheck/controller/SubmissionController.java
@@ -5,9 +5,10 @@ import com.buzzardsview.readitcheck.data.UserRepository;
 import com.buzzardsview.readitcheck.model.Question;
 import com.buzzardsview.readitcheck.model.Submission;
 import com.buzzardsview.readitcheck.model.User;
-import com.buzzardsview.readitcheck.model.dto.CommentDto;
-import com.buzzardsview.readitcheck.model.dto.QuestionDto;
-import com.buzzardsview.readitcheck.model.dto.SubmissionDto;
+import com.buzzardsview.readitcheck.model.dto.QuestionPostDto;
+import com.buzzardsview.readitcheck.model.dto.SubmissionGetDto;
+import com.buzzardsview.readitcheck.model.dto.SubmissionPostDto;
+import com.buzzardsview.readitcheck.security.AppTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,20 +29,33 @@ public class SubmissionController {
     private UserRepository userRepository;
 
     @GetMapping("/{id}")
-    public Submission getOne(@PathVariable Integer id) {
-        return submissionRepository.getById(id).orElseThrow();
+    public SubmissionGetDto getOne(@PathVariable Integer id, ServletRequest servletRequest) {
+
+        Submission submission = submissionRepository.getById(id).orElseThrow();
+        User user = userRepository.findById(AppTokenProvider.getCurrentUserId(servletRequest)).orElseThrow();
+
+        boolean currentUserApproved = user == null ? false : submission.getApprovedUsers().contains(user);
+
+        return new SubmissionGetDto(
+                submission.getId(),
+                submission.getUser(),
+                submission.getTitle(),
+                submission.getLink(),
+                submission.getQuestions(),
+                submission.getComments(),
+                submission.getTimestamp(),
+                currentUserApproved
+        );
     }
 
     @PostMapping
-    public void newSubmission(@RequestBody SubmissionDto submissionDto, ServletRequest servletRequest) {
+    public void newSubmission(@RequestBody SubmissionPostDto submissionDto, ServletRequest servletRequest) {
 
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String userId = (String) request.getAttribute("userId");
-        User user = userRepository.findById(Long.parseLong(userId)).orElseThrow();
+        User user = userRepository.findById(AppTokenProvider.getCurrentUserId(servletRequest)).orElseThrow();
         Submission newSubmission = new Submission(user, submissionDto.getTitle(), submissionDto.getLink());
 
         List<Question> questions = new ArrayList<>();
-        for (QuestionDto questionDto : submissionDto.getQuestions()) {
+        for (QuestionPostDto questionDto : submissionDto.getQuestions()) {
             questions.add(new Question(questionDto.getQuestion(), questionDto.getAnswer(), newSubmission));
         }
 
