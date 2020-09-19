f26c7a3c49bebe08b5d5ab2284595367d4d0e9d0
cbuzzard <corwin1998@gmail.com>
2020-09-18 23:39:26 -0400

    login permissions

--- src/main/java/com/buzzardsview/readitcheck/config/SecurityConfiguration.java
+++ src/main/java/com/buzzardsview/readitcheck/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.buzzardsview.readitcheck.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,5 +13,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors();
+        httpSecurity
+                .csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/login").permitAll();
     }
 }
