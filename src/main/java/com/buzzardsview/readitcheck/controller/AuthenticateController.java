91dbcef15157fda637b3ebc61568ebeef515a49b
cbuzzard <corwin1998@gmail.com>
2020-09-18 23:39:53 -0400

    better validation

--- src/main/java/com/buzzardsview/readitcheck/controller/AuthenticateController.java
+++ src/main/java/com/buzzardsview/readitcheck/controller/AuthenticateController.java
@@ -1,15 +1,27 @@
 package com.buzzardsview.readitcheck.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
-import javax.servlet.ServletRequest;
+import static com.buzzardsview.readitcheck.security.AppTokenProvider.getUserFromToken;
 
-@RestController("rest/authenticate")
+@RestController
+@RequestMapping("rest/authenticate")
 public class AuthenticateController {
 
-    public boolean authenticateUser(ServletRequest servletRequest) {
-        return true;
+    @GetMapping("")
+    public boolean authenticateUser(HttpServletRequest request, ServletResponse servletResponse) {
+        Optional<String> userFromToken;
+        try {
+            userFromToken = getUserFromToken(request);
+            return userFromToken.isPresent();
+        } catch (Throwable err) {
+            return false;
+        }
     }
 
 }
