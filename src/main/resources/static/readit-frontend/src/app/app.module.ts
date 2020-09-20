98d052e39687c237f36b97f08778f42e4db78c04
cbuzzard <corwin1998@gmail.com>
2020-09-19 23:46:59 -0400

    imports

--- src/main/resources/static/readit-frontend/src/app/app.module.ts
+++ src/main/resources/static/readit-frontend/src/app/app.module.ts
@@ -6,17 +6,40 @@ import { LoginButtonComponent } from './login-button/login-button.component';
 import { HttpInterceptorService } from './service/interceptor/http-interceptor.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { NewSubmissionComponent } from './new-submission/new-submission.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//TODO organize imports
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginButtonComponent
+    LoginButtonComponent,
+    HeaderComponent,
+    NewSubmissionComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true}],
   bootstrap: [AppComponent]
