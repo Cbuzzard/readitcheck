0a4626b6497be679e242ae5b34b1779a4abf6bae
cbuzzard <corwin1998@gmail.com>
2020-09-20 22:54:26 -0400

    wires to backend

---
+++ src/main/resources/static/readit-frontend/src/app/service/rest/rest.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { SubmissionPost } from 'src/app/dto/submission-post';
+import { UrlService } from '../url/url.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private url: UrlService, private http: HttpClient) { }
+
+  postSubmission(submissionPost: SubmissionPost) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post(`${this.url.backend}/rest/submission`, JSON.stringify(submissionPost), httpOptions);
+  }
+
+  getSubmission(id: number) {
+    return this.http.get(`${this.url.backend}/rest/submission/${id}`)
+  } 
+
+}
