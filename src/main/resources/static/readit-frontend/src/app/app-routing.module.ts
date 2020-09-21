306fb8a4fa10f4fb4c5f7bd16cbbdf03c089b40d
cbuzzard <corwin1998@gmail.com>
2020-09-20 22:55:15 -0400

    added url routes

---
+++ src/main/resources/static/readit-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SubmissionComponent } from './submission/submission.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'submission/:id', component: SubmissionComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
