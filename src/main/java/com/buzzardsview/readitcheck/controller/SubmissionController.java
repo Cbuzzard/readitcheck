db74b93278e1f20e5ee24de3152e56723d61bc33
cbuzzard <corwin1998@gmail.com>
2020-09-21 23:59:15 -0400

    fixed recursion issues

--- src/main/java/com/buzzardsview/readitcheck/controller/SubmissionController.java
+++ src/main/java/com/buzzardsview/readitcheck/controller/SubmissionController.java
@@ -6,15 +6,19 @@ import com.buzzardsview.readitcheck.data.UserRepository;
 import com.buzzardsview.readitcheck.model.Question;
 import com.buzzardsview.readitcheck.model.Submission;
 import com.buzzardsview.readitcheck.model.User;
-import com.buzzardsview.readitcheck.model.dto.QuestionPostDto;
-import com.buzzardsview.readitcheck.model.dto.SubmissionGetDto;
-import com.buzzardsview.readitcheck.model.dto.SubmissionPostDto;
+import com.buzzardsview.readitcheck.model.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletRequest;
+import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("rest/submission")
@@ -72,8 +76,20 @@ public class SubmissionController {
     }
 
     @GetMapping
-    public List<Submission> getAll() {
-        return submissionRepository.findAll();
+    public List<SubmissionForListDto> list(@RequestParam(name = "page", defaultValue = "0") int page,
+                                           @RequestParam(name = "size", defaultValue = "10") int size) {
+
+        Page<Submission> submissionPage = submissionRepository.findAll(PageRequest.of(page, size, Sort.by("timestamp")));
+        List<SubmissionForListDto> submissionsForListDto = submissionPage.getContent().stream().map(s ->
+                        new SubmissionForListDto(
+                                s.getId(),
+                                s.getTitle(),
+                                s.getLink(),
+                                s.getTimestamp(),
+                                s.getUser().getSimpleUser()
+                        )).collect(Collectors.toList());
+
+        return submissionsForListDto;
     }
 
 }
