3f080a2dcd0ae4a138ae933381ab5f889787d7d9
cbuzzard <corwin1998@gmail.com>
2020-09-21 23:59:22 -0400

    fixed recursion issues

--- src/main/java/com/buzzardsview/readitcheck/data/SubmissionRepository.java
+++ src/main/java/com/buzzardsview/readitcheck/data/SubmissionRepository.java
@@ -1,14 +1,22 @@
 package com.buzzardsview.readitcheck.data;
 
 import com.buzzardsview.readitcheck.model.Submission;
+import com.buzzardsview.readitcheck.model.dto.SubmissionForListDto;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Integer> {
 
     @EntityGraph("submission-entity-graph")
     Optional<Submission> getById(Integer id);
+
+    @Override
+//    @EntityGraph("submission-entity-graph")
+    List<Submission> findAll();
 
 }
