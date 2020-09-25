0e608044a74dc8d7f609657f10c4380019d67f36
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:43:50 -0400

    custom exception

---
+++ src/main/java/com/buzzardsview/readitcheck/model/exception/SubmissionNotFoundException.java
@@ -0,0 +1,13 @@
+package com.buzzardsview.readitcheck.model.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class SubmissionNotFoundException extends RuntimeException {
+
+    public SubmissionNotFoundException(String message) {
+        super(message);
+    }
+}
