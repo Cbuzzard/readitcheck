231201e8dd9db77c7f2294bb21b06f664349deca
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:54:30 -0400

    better loginstatus check

--- src/main/resources/static/readit-frontend/src/app/new-submission/new-submission.component.ts
+++ src/main/resources/static/readit-frontend/src/app/new-submission/new-submission.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatAccordion} from '@angular/material/expansion';
-import { FormBuilder } from '@angular/forms';
-import {SubmissionPost} from '../dto/submission-post'
-import {Question} from '../dto/question'
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user/user.service';
 import { RestService } from "../service/rest/rest.service"
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { SubmissionFormDialogComponent } from '../submission-form-dialog/submission-form-dialog.component';
 
 
@@ -14,15 +10,19 @@ import { SubmissionFormDialogComponent } from '../submission-form-dialog/submiss
   templateUrl: './new-submission.component.html',
   styleUrls: ['./new-submission.component.css']
 })
-export class NewSubmissionComponent implements OnInit {
+export class NewSubmissionComponent implements OnInit{
 
   loginStatus: boolean;
 
   constructor(private user: UserService, private rest: RestService, public dialog: MatDialog) {
-    this.user.loginStatus.subscribe(res => this.loginStatus = res)
+    
   }
 
   ngOnInit(): void {
+    this.user.loginStatus.subscribe(res => {
+      this.loginStatus = res;
+    })
+    this.user.refreshStatus();
   }
 
   openDialog() {
