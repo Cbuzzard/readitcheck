acca86d3bb0ebda6cb43812a203d2f8383e866ec
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:52:42 -0400

    infinite scroll

--- src/main/resources/static/readit-frontend/src/app/home/home.component.ts
+++ src/main/resources/static/readit-frontend/src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { SubmissionForList } from '../dto/submission-for-list';
+import { RestService } from '../service/rest/rest.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  page: number = 0;
+  submissions: Array<SubmissionForList> = []
+
+  constructor(private rest: RestService) {
+    this.getSubmissions()
+  }
 
   ngOnInit(): void {
+  }
+
+  onScroll() {
+    this.getSubmissions()
+  }
+
+  getSubmissions() {
+    this.rest.getSubmissions(this.page).subscribe((res: Array<SubmissionForList>) => {
+      this.submissions.push(...res)
+    })
+    this.page++
   }
 
 }
