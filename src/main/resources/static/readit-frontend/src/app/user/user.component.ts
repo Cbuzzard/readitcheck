2e93fa0ceba38f3cc135edfd6ff2132b239315bf
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:48:08 -0400

    basic user page

--- src/main/resources/static/readit-frontend/src/app/user/user.component.ts
+++ src/main/resources/static/readit-frontend/src/app/user/user.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestService } from '../service/rest/rest.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user;
+  selectedTab = 0;
+
+  constructor(private rest: RestService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() {
+    this.route.params.subscribe(param => {
+      this.rest.getUser(param.id).subscribe(res => {
+        this.user = res
+      })
+    })
+  }
+
+  tabChange(tab) {
+    this.selectedTab = tab.index;
   }
 
 }
