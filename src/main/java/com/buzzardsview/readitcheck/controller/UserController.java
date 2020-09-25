6567ba4ce1cdae40c16ec52efd08fac11869c5a0
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:43:38 -0400

    better get user controller

--- src/main/java/com/buzzardsview/readitcheck/controller/UserController.java
+++ src/main/java/com/buzzardsview/readitcheck/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.buzzardsview.readitcheck.controller;
 
 import com.buzzardsview.readitcheck.data.UserRepository;
+import com.buzzardsview.readitcheck.model.Comment;
+import com.buzzardsview.readitcheck.model.Submission;
 import com.buzzardsview.readitcheck.model.User;
+import com.buzzardsview.readitcheck.model.dto.comment.CommentForListDto;
+import com.buzzardsview.readitcheck.model.dto.submission.SubmissionForListDto;
+import com.buzzardsview.readitcheck.model.dto.user.UserGetDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("rest/user")
@@ -18,8 +27,33 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/{id}")
-    public User getOne(@PathVariable String id) {
-        return userRepository.getByGoogleId(id).orElseThrow();
+    public UserGetDto getOne(@PathVariable String id) {
+        User user = userRepository.getByGoogleId(id).orElseThrow();
+
+        List<CommentForListDto> comments = user.getComments().stream().map(Comment::getCommentForUserList)
+                .collect(Collectors.toList());
+
+        Comparator<CommentForListDto> compareComByTimeStamp = Comparator.comparing(CommentForListDto::getTimestamp)
+                .reversed();
+
+        comments.sort(compareComByTimeStamp);
+
+        List<SubmissionForListDto> submissions = user.getSubmissions().stream().map(Submission::getSubmissionForList)
+                .collect(Collectors.toList());
+
+        Comparator<SubmissionForListDto> compareSubByTimeStamp = Comparator.comparing(SubmissionForListDto::getTimestamp)
+                .reversed();
+
+        submissions.sort(compareSubByTimeStamp);
+
+        return new UserGetDto(
+                user.getGoogleId(),
+                user.getUsername(),
+                user.getPicture(),
+                comments,
+                submissions
+        );
+
     }
 
 }
