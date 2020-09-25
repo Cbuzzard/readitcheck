17627e8401f4100dae1dba5493c1d0ec4983c1e8
cbuzzard <corwin1998@gmail.com>
2020-09-24 21:55:44 -0400

    added endpoints

--- src/main/resources/static/readit-frontend/src/app/service/rest/rest.service.ts
+++ src/main/resources/static/readit-frontend/src/app/service/rest/rest.service.ts
@@ -8,27 +8,49 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class RestService {
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   constructor(private url: UrlService, private http: HttpClient) { }
 
-  postSubmission(submissionPost: SubmissionPost) {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    }
-    return this.http.post(`${this.url.backend}/rest/submission`, JSON.stringify(submissionPost), httpOptions);
+  postSubmission(submissionPost) {
+    console.log(submissionPost.title)
+    return this.http.post(`${this.url.backend}/rest/submission`, JSON.stringify(submissionPost), this.httpOptions);
   }
 
   getSubmission(id: number) {
-    return this.http.get(`${this.url.backend}/rest/submission/${id}`)
+    return this.http.get(`${this.url.backend}/rest/submission/${id}`);
   }
 
   getSubmissions(page: number) {
-    return this.http.get(`${this.url.backend}/rest/submission?page=${page}`)
+    return this.http.get(`${this.url.backend}/rest/submission?page=${page}`);
   }
 
   getLinkPreview(url: string) {
-    return this.http.get(`${this.url.backend}/rest/link?url=${url}`)
+    return this.http.get(`${this.url.backend}/rest/link?url=${url}`);
+  }
+
+  checkAnswer(id: number, answer: string) {
+    return this.http.post(`${this.url.backend}/rest/question/${id}`, answer, this.httpOptions);
+  }
+
+  postComment(submissionId, content) {
+    return this.http.post(`${this.url.backend}/rest/submission/${submissionId}/comment`, content, this.httpOptions);
+  }
+
+  getUser(id: number) {
+    return this.http.get(`${this.url.backend}/rest/user/${id}`);
+  }
+
+  deleteComment(submissionId, commentId) {
+    return this.http.delete(`${this.url.backend}/rest/submission/${submissionId}/comment/${commentId}`);
+  }
+
+  deleteSubmission(submissionId) {
+    return this.http.delete(`${this.url.backend}/rest/submission/${submissionId}`);
   }
 
 }
