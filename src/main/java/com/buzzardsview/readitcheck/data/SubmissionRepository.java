027557127eaffe5250419c1207e9f0f62957dfff
cbuzzard <corwin1998@gmail.com>
2020-09-27 23:42:22 -0400

    cleanup

--- src/main/java/com/buzzardsview/readitcheck/data/SubmissionRepository.java
+++ src/main/java/com/buzzardsview/readitcheck/data/SubmissionRepository.java
@@ -4,16 +4,11 @@ import com.buzzardsview.readitcheck.model.Submission;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Integer> {
 
     @EntityGraph("submission-entity-graph")
     Optional<Submission> getById(Integer id);
-
-    @Override
-//    @EntityGraph("submission-entity-graph")
-    List<Submission> findAll();
 
 }
--- src/main/java/com/buzzardsview/readitcheck/data/UserRepository.java
+++ src/main/java/com/buzzardsview/readitcheck/data/UserRepository.java
@@ -1,7 +1,6 @@
 package com.buzzardsview.readitcheck.data;
 
 import com.buzzardsview.readitcheck.model.User;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
